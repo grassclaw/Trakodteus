ffacff93b13a1b7ebebf2933b22eac7ee64962fa
shadowlaq <escamillaa@email.arizona.edu>
2020-04-17 14:07:27 -0700

    adding click events to portfolio page

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,7 +1,7 @@
 // portfolio.html Page Links
     // Object of Links
-    var IndexObj = {1:"", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:""};
+    var IndexObj = {1:"shadowlaqs-first.html", 2:"", 3:"", 4:"", 5:"", 6:"", 7:"", 8:""};
 
-    $("#tutorials").on("click",function(){
-            window.location.href='https://shadowlaq.github.io/DraKodeus'+IndexObj[webPath];
+    $(".tutorials").on("click",function(){
+            window.location.href='https://shadowlaq.github.io/DraKodeus/tutorial1/'+IndexObj[webPath];
     });
